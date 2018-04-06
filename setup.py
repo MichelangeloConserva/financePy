f9c6ee55bf26c08cb4977ff59337d364a222cb4c
Michelangelo Conserva <michelangelo.conserva@protonmail.com>
2018-04-06 20:10:59 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+try:
+    with open('LICENSE.txt', 'r') as f:
+        _license = f.read()
+except:
+    _license = ''
+
+try:
+    with open('README.md', 'r') as f:
+        _readme = f.read()
+except:
+    _readme = ''
+
+setup(
+  name='prova',
+  packages=find_packages(exclude=['aaaaa']),
+  version='0.2.1',
+  description='+ una provae',
+  author='Michelangelo Conserva',
+  author_email='mom',
+  license=_license,
+  long_description=_readme,
+  url='no',
+  download_url='https://github.com/MichelangeloConserva/prova/releases',
+  install_requires=['setuptools', 'pandas', 'requests'],
+)
